c21eae23248a9d1cda9a372fbadfcb8ef1b2dbc4
Mandela Puepi <pmandel0001@yahoo.fr>
2023-03-04 20:53:44 +0100

    added a fake data file

---
+++ frontend/src/data.js
@@ -0,0 +1,70 @@
+const data={
+    products:[
+        {
+            _id:1,
+            name:'Nike Slim Shirts',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price:120,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:"high quality product"
+        },
+        {
+            _id:2,
+            name:'Adidas Fit Shirts',
+            category:'Shirts',
+            image:'/images/p2.jpg',
+            price:100,
+            brand:'Nike',
+            rating:4.0,
+            numReviews:10,
+            description:"high quality product"
+        },
+        {
+            _id:3,
+            name:'Lacoste Free Shirts',
+            category:'Shirts',
+            image:'/images/p3.jpg',
+            price:220,
+            brand:'Lacoste',
+            rating:4.8,
+            numReviews:17,
+            description:"high quality product"
+        },
+        {
+            _id:4,
+            name:'Nike Slim Pant',
+            category:'Pants',
+            image:'/images/p4.jpg',
+            price:78,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:"high quality product"
+        },
+        {
+            _id:5,
+            name:'Pume  Slim Pants',
+            category:'Pants',
+            image:'/images/p1.jpg',
+            price:65,
+            brand:'Puma',
+            rating:4.5,
+            numReviews:10,
+            description:"high quality product"
+        },
+        {
+            _id:6,
+            name:'Adidas Fit Pant',
+            category:'Pants',
+            image:'/images/p6.jpg',
+            price:139,
+            brand:'Adidas',
+            rating:4.5,
+            numReviews:15,
+            description:"high quality product"
+        }
+    ]
+}
