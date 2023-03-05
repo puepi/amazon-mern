baa141c53a0fbb47d1b9b9f8e1967d678eb5edc2
Mandela Puepi <pmandel0001@yahoo.fr>
2023-03-05 07:47:18 +0100

    added nodemon dependency and included start script for the backend

---
+++ backend/data.js
@@ -0,0 +1,78 @@
+const data={
+    products:[
+        {
+            _id:1,
+            name:'Nike Slim Shirts',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price:120,
+            countInStock:10,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:9,
+            description:"high quality product"
+        },
+        {
+            _id:2,
+            name:'Adidas Fit Shirts',
+            category:'Shirts',
+            image:'/images/p2.jpg',
+            price:100,
+            countInStock:8,
+            brand:'Nike',
+            rating:4.0,
+            numReviews:10,
+            description:"high quality product"
+        },
+        {
+            _id:3,
+            name:'Lacoste Free Shirts',
+            category:'Shirts',
+            image:'/images/p3.jpg',
+            price:220,
+            countInStock:7,
+            brand:'Lacoste',
+            rating:2.8,
+            numReviews:17,
+            description:"high quality product"
+        },
+        {
+            _id:4,
+            name:'Nike Slim Pant',
+            category:'Pants',
+            image:'/images/p4.jpg',
+            price:78,
+            countInStock:5,
+            brand:'Nike',
+            rating:1.5,
+            numReviews:12,
+            description:"high quality product"
+        },
+        {
+            _id:5,
+            name:'Pume  Slim Pants',
+            category:'Pants',
+            image:'/images/p1.jpg',
+            price:65,
+            countInStock:12,
+            brand:'Puma',
+            rating:4.5,
+            numReviews:7,
+            description:"high quality product"
+        },
+        {
+            _id:6,
+            name:'Adidas Fit Pant',
+            category:'Pants',
+            image:'/images/p6.jpg',
+            price:139,
+            countInStock:0,
+            brand:'Adidas',
+            rating:5,
+            numReviews:15,
+            description:"high quality product"
+        }
+    ]
+}
+
+export default data
