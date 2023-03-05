64f1e2de3d9b24f4e610a436e73e091be5f87096
Mandela Puepi <pmandel0001@yahoo.fr>
2023-03-05 08:16:20 +0100

    added proxy key to initiate a proxy server in frontend package.json

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import data from './data.js'
-
+const port=process.env.PORT || 5000
 const app=express()
 
 app.get('/api/products',(req,res)=>{
@@ -11,6 +11,6 @@ app.get('/',(req,res)=>{
     res.send('Server is ready on port')
 })
 
-app.listen(5000,()=>{
-    console.log('Serve at http://localhost:5000/')
+app.listen(port,()=>{
+    console.log(`Serve at http://localhost:${port}/`)
 })
