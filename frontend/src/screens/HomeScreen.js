7a086f55464a4da70dd7312a710eaa45921d70dd
Mandela Puepi <pmandel0001@yahoo.fr>
2023-03-06 23:02:07 +0100

    added error and loading handler

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,24 +1,39 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import Product from '../components/Product'
+import LoadingBox from '../components/LoadingBox'
+import MessageBox from '../components/MessageBox'
 
 const HomeScreen = () => {
   const [products,setProducts]=useState([])
+  const [loading,setLoading]=useState(false)
+  const [error,setError]=useState(false)
   useEffect(()=>{
     const fetchData=async ()=>{
-      const {data}=await axios.get('/api/products')
-      setProducts(data)
+      try{
+        setLoading(true)
+        const {data}=await axios.get('/api/products')
+        setLoading(false)
+        setProducts(data)
+      }catch(err){
+          setError(err.message)
+          setLoading(false)
+      }
+      
     }
     fetchData()
   },[])
   return (
-    <div className="row center">
-        {
-            products.map((product)=>(
-                <Product  key={product._id} product={product}/>
-            ))
-        }
-    </div>
+    
+      loading ? <LoadingBox /> :
+      error ? <MessageBox variant="danger">{error}</MessageBox>:
+        <div className="row center">
+          {
+              products.map((product)=>(
+                  <Product  key={product._id} product={product}/>
+              ))
+          }
+        </div>
   )
 }
 
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -46,7 +46,7 @@ const ProductScreen = ({match}) => {
                       {
                         product.countInStock > 0 
                         ? (<span className='success'>In Stock</span>)
-                        : (<span className='error'>Unavailable</span>)
+                        : (<span className='danger'>Unavailable</span>)
                       }
                     </div>
                   </div>
